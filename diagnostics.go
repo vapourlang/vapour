0e7841765f1ee2da96a9d7afaf222239c904a47b
John Coene <jcoenep@gmail.com>
2024-06-23 19:37:56 +0200

    debug: hello world test

--- diagnostics.go
+++ diagnostics.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"github.com/tliron/commonlog"
 	"github.com/tliron/glsp"
 	protocol "github.com/tliron/glsp/protocol_3_16"
 )
 
-func (d *doctor) publishDiagnostics(context *glsp.Context, params *protocol.PublishDiagnosticsParams) (interface{}, error) {
-	var diagnostics []protocol.Diagnostic
-
-	return diagnostics, nil
-}
-
 func (d *doctor) textDocumentDidOpen(context *glsp.Context, params *protocol.DidOpenTextDocumentParams) error {
-	commonlog.NewInfoMessage(0, "Document open...")
+	context.Notify(protocol.ServerWindowShowMessage, protocol.ShowMessageParams{
+		Message: "Hello World",
+		Type:    protocol.MessageTypeInfo,
+	})
 	return nil
 }
 
 func (d *doctor) textDocumentDidChange(context *glsp.Context, params *protocol.DidChangeTextDocumentParams) error {
+	context.Notify(protocol.ServerWindowShowMessage, protocol.ShowMessageParams{
+		Message: "Hello World Init",
+		Type:    protocol.MessageTypeInfo,
+	})
 	return nil
 }
