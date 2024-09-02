a9495ae1e15d16af45c079296100faa557f4ae9c
John Coene <jcoenep@gmail.com>
2024-09-03 01:33:32 +0200

    fix: trim unused vars

--- vapour.go
+++ vapour.go
@@ -2,18 +2,13 @@ package main
 
 import (
 	"github.com/devOpifex/vapour/lexer"
-	protocol "github.com/tliron/glsp/protocol_3_16"
-	"github.com/tliron/glsp/server"
 )
 
 type vapour struct {
-	name     string
-	version  string
-	handler  *protocol.Handler
-	server   *server.Server
-	root     *string
-	files    lexer.Files
-	combined []byte
+	name    string
+	version string
+	root    *string
+	files   lexer.Files
 }
 
 func New() *vapour {
