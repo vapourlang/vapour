6770761530f01a995b3f3e1e084da06a2db6f157
John Coene <jcoenep@gmail.com>
2024-09-03 03:08:36 +0200

    feat: LSP config closes #26

--- lsp/lsp.go
+++ lsp/lsp.go
@@ -1,7 +1,10 @@
 package lsp
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"path"
 	"path/filepath"
 	"strings"
 
@@ -16,16 +19,78 @@ import (
 
 type LSP struct {
 	files []lexer.File
+	conf  *config
 }
 
 type walkParams struct {
 	TextDocument protocol.DocumentUri
 }
 
+type config struct {
+	When     []string `json:"when"`
+	Severity []string `json:"severity"`
+}
+
 var handler protocol.Handler
 var version string = "0.0.1"
 var code protocol.IntegerOrString = protocol.IntegerOrString{Value: 2}
 var src string = "Vapour"
+var file string = ".vapour"
+
+func makeConfigPath(conf string) string {
+	dirname, err := os.UserHomeDir()
+
+	if err != nil {
+		return ""
+	}
+
+	return path.Join(dirname, conf)
+}
+
+func hasConfig(conf string) bool {
+	p := makeConfigPath(conf)
+
+	if p == "" {
+		return false
+	}
+
+	_, err := os.Stat(p)
+
+	return !os.IsNotExist(err)
+}
+
+func readConfig(conf string) (*config, bool) {
+	configuration := &config{}
+
+	if !hasConfig(conf) {
+		return configuration, false
+	}
+
+	p := makeConfigPath(conf)
+	data, err := os.ReadFile(p)
+
+	if err != nil {
+		return configuration, false
+	}
+
+	err = json.Unmarshal(data, configuration)
+
+	if err != nil {
+		return configuration, false
+	}
+
+	return configuration, true
+}
+
+func contains(value string, values []string) bool {
+	for _, v := range values {
+		if v == value {
+			return true
+		}
+	}
+
+	return false
+}
 
 func New() *LSP {
 	return &LSP{
@@ -36,15 +101,38 @@ func New() *LSP {
 func Run(tcp bool, port string) {
 	l := New()
 
+	conf, ok := readConfig(file)
+
+	l.conf = conf
+
 	handler = protocol.Handler{
-		Initialize:            l.initialize,
-		Initialized:           l.initialized,
-		Shutdown:              l.shutdown,
-		SetTrace:              l.setTrace,
-		TextDocumentDidOpen:   l.textDocumentDidOpen,
-		TextDocumentDidSave:   l.textDocumentDidSave,
-		TextDocumentDidClose:  l.textDocumentDidClose,
-		TextDocumentDidChange: l.textDocumentDidChange,
+		Initialize:  l.initialize,
+		Initialized: l.initialized,
+		Shutdown:    l.shutdown,
+		SetTrace:    l.setTrace,
+	}
+
+	if !ok {
+		l.conf = &config{
+			When:     []string{"open", "save", "close", "text"},
+			Severity: []string{"fatal", "warn", "info", "hint"},
+		}
+	}
+
+	if contains("open", conf.When) {
+		handler.TextDocumentDidOpen = l.textDocumentDidOpen
+	}
+
+	if contains("save", conf.When) {
+		handler.TextDocumentDidSave = l.textDocumentDidSave
+	}
+
+	if contains("close", conf.When) {
+		handler.TextDocumentDidClose = l.textDocumentDidClose
+	}
+
+	if contains("change", conf.When) {
+		handler.TextDocumentDidChange = l.textDocumentDidChange
 	}
 
 	server := server.NewServer(&handler, "Vapour", false)
@@ -132,7 +220,7 @@ func (l *LSP) walkFiles(context *glsp.Context, params *walkParams) error {
 	le.Run()
 
 	if le.HasError() {
-		diagnostics = addError(diagnostics, le.Errors, file)
+		diagnostics = addError(diagnostics, le.Errors, file, l.conf.Severity)
 		ds := protocol.PublishDiagnosticsParams{
 			URI:         params.TextDocument,
 			Diagnostics: diagnostics,
@@ -146,7 +234,7 @@ func (l *LSP) walkFiles(context *glsp.Context, params *walkParams) error {
 	prog := p.Run()
 
 	if p.HasError() {
-		diagnostics = addError(diagnostics, p.Errors(), file)
+		diagnostics = addError(diagnostics, p.Errors(), file, l.conf.Severity)
 		ds := protocol.PublishDiagnosticsParams{
 			URI:         params.TextDocument,
 			Diagnostics: diagnostics,
@@ -159,7 +247,7 @@ func (l *LSP) walkFiles(context *glsp.Context, params *walkParams) error {
 	w := walker.New()
 	w.Walk(prog)
 
-	diagnostics = addError(diagnostics, w.Errors(), file)
+	diagnostics = addError(diagnostics, w.Errors(), file, l.conf.Severity)
 	ds := protocol.PublishDiagnosticsParams{
 		URI:         params.TextDocument,
 		Diagnostics: diagnostics,
@@ -196,9 +284,33 @@ func (l *LSP) textDocumentDidChange(context *glsp.Context, params *protocol.DidC
 	return l.walkFiles(context, p)
 }
 
-func addError(ds []protocol.Diagnostic, ns diagnostics.Diagnostics, file string) []protocol.Diagnostic {
+func validSeverity(severity diagnostics.Severity, valid []string) bool {
+	if severity == 0 && contains("fatal", valid) {
+		return true
+	}
+
+	if severity == 1 && contains("warn", valid) {
+		return true
+	}
+
+	if severity == 2 && contains("hint", valid) {
+		return true
+	}
+
+	if severity == 3 && contains("info", valid) {
+		return true
+	}
+
+	return false
+}
+
+func addError(ds []protocol.Diagnostic, ns diagnostics.Diagnostics, file string, include []string) []protocol.Diagnostic {
 	for _, e := range ns {
 		if e.Token.File != file {
+			continue
+		}
+
+		if !validSeverity(e.Severity, include) {
 			continue
 		}
 
